b798cfbe52bc2109d28d8dc7fdd0c042ff7ea490
Ofek Doitch <Ofek@WeedonAndScott.com>
2024-11-04 02:49:51 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+      });
+    in
+    {
+      overlays.default = final: prev: rec {
+        nodejs = prev.nodejs;
+        yarn = (prev.yarn.override { inherit nodejs; });
+      };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Gleam
+            gleam
+
+            # Erlang
+            erlang_27
+            rebar3
+
+            # Node
+            node2nix
+            nodejs
+            nodePackages.pnpm
+
+            # Lustre devtools
+            inotify-tools
+          ];
+        };
+      });
+    };
+}
